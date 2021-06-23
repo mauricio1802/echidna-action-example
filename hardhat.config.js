eb0b14dedcbdbfdf1cbd22e44932f68075933963
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-06-23 17:35:22 -0400

    Bring project files

---
+++ hardhat.config.js
@@ -0,0 +1,18 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.6.0"
+      },
+      {
+        version: "0.8.0"
+      },
+      {
+        version: "0.7.3"
+      }
+    ]
+  }
+};
