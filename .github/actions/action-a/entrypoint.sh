e9d69c29a57b151323753a836c9af78115fd553c
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-06-23 19:46:39 -0400

    Check crytic-version and try to run

--- .github/actions/action-a/entrypoint.sh
+++ .github/actions/action-a/entrypoint.sh
@@ -3,4 +3,6 @@
 sh -c "ls -a"
 sh -c "echo $PWD"
 sh -c "echidna-test --version"
-# sh -c "echidna-test . --contract Fallback"
+sh -c "crytic-compile --version"
+sh -c "npm install"
+sh -c "echidna-test . --contract Fallback"
