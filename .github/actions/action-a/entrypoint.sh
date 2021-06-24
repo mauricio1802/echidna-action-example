792f4dbd3ea65ccd8a9dc1c5e225dcec0f67cd69
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-06-23 22:35:07 -0400

    Use config when running echidna

--- .github/actions/action-a/entrypoint.sh
+++ .github/actions/action-a/entrypoint.sh
@@ -2,6 +2,6 @@
 bash -c "python3 -m pip install crytic-compile"
 bash -c "python3 -m pip install slither-analyzer"
 bash -c "npm install"
-bash -c "echidna-test . --contract Fallback"
+bash -c "echidna-test . --contract Fallback --config echidna.config.yml"
 bash -c "ls -a"
 bash -c "ls -a crytic-export"
