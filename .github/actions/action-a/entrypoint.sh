457a89f946fe479bcc385486345fd466b2dbdad5
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-06-23 21:51:25 -0400

    Install correct slither

--- .github/actions/action-a/entrypoint.sh
+++ .github/actions/action-a/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 bash -c "python3 -m pip install crytic-compile"
-bash -c "python3 -m pip install slither"
-bash -c "python3 -m pip install pygame"
+bash -c "python3 -m pip install slither-analyzer"
 bash -c "npm install"
 bash -c "echidna-test . --contract Fallback"
