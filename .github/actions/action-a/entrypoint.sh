ff340febf4a6301736241d10f05948628c47af9d
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-06-24 09:57:55 -0400

    Test env variables

--- .github/actions/action-a/entrypoint.sh
+++ .github/actions/action-a/entrypoint.sh
@@ -2,4 +2,5 @@
 bash -c "python3 -m pip install crytic-compile"
 bash -c "python3 -m pip install slither-analyzer"
 bash -c "npm install"
+bash -c "echo $CONTRACT_NAME"
 bash -c "echidna-test . --contract $CONTRACT_NAME --config echidna.config.yml > echidna.result.json"
