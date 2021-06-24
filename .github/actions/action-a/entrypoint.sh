c12775bf9372ac4f2665474aba1f2431c6206a51
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-06-23 21:48:25 -0400

    Intall pygame

--- .github/actions/action-a/entrypoint.sh
+++ .github/actions/action-a/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 bash -c "python3 -m pip install crytic-compile"
 bash -c "python3 -m pip install slither"
+bash -c "python3 -m pip install pygame"
 bash -c "npm install"
 bash -c "echidna-test . --contract Fallback"
