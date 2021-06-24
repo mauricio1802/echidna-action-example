dcbc00dfad70ebb68e70d573de6dbb1eef057f77
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-06-24 10:41:20 -0400

    Use specific config file

--- .github/actions/action-a/entrypoint.sh
+++ .github/actions/action-a/entrypoint.sh
@@ -2,4 +2,4 @@
 bash -c "python3 -m pip install crytic-compile"
 bash -c "python3 -m pip install slither-analyzer"
 bash -c "npm install"
-bash -c "echidna-test . --contract $INPUT_CONTRACT_NAME --config echidna.config.yml > echidna.result.json"
+bash -c "echidna-test . --contract $INPUT_CONTRACT_NAME --config $INPUT_CONTRACTNAME.config.yml > echidna.result.json"
